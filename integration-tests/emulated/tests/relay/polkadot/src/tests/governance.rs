86a488e4cfaf2706ac5a2f63a9a1d76ee2295c84
Alexandre Baldé <alexandre.balde@parity.io>
2025-03-03 16:01:49 +0000

    Governance XCM integration tests (WIP)

--- integration-tests/emulated/tests/relay/polkadot/src/tests/governance.rs
+++ integration-tests/emulated/tests/relay/polkadot/src/tests/governance.rs
@@ -16,12 +16,21 @@
 use crate::*;
 use emulated_integration_tests_common::{accounts::{ALICE, BOB, CHARLIE, DAVE, EVE}, xcm_emulator::log};
 
-use frame_support::traits::{schedule::DispatchTime, StorePreimage};
+use frame_support::{
+	sp_runtime::traits::Dispatchable,
+	traits::{schedule::DispatchTime, StorePreimage}
+};
 
-use pallet_conviction_voting::{AccountVote, Conviction, Vote};
-use pallet_referenda::ReferendumInfoFor;
+use pallet_conviction_voting::Tally;
+use pallet_referenda::{ReferendumInfoFor, TallyOf};
 
-use polkadot_runtime::{governance::pallet_custom_origins::Origin, ConvictionVoting, Preimage, Referenda};
+use polkadot_runtime::{
+	governance::{
+		pallet_custom_origins::{self, Origin},
+		ReferendumCanceller,
+		ReferendumKiller
+	}, ConvictionVoting, Preimage, Referenda, RuntimeCall, RuntimeOrigin
+};
 use polkadot_runtime_common::impls::VersionedLocatableAsset;
 
 use polkadot_runtime_constants::currency::UNITS as DOT;
@@ -31,166 +40,117 @@ use frame_system::Pallet as System;
 #[test]
 fn referendum_lifecycle() {
 	let polkadot_alice = Polkadot::account_id_of(ALICE);
-	let polkadot_bob = Polkadot::account_id_of(BOB);
-	let polkadot_charlie = Polkadot::account_id_of(CHARLIE);
-	let polkadot_dave = Polkadot::account_id_of(DAVE);
-	let polkadot_eve = Polkadot::account_id_of(EVE);
 
-	Polkadot::execute_with(|| {
-		type Runtime = <Polkadot as Chain>::Runtime;
-		type RuntimeCall = <Polkadot as Chain>::RuntimeCall;
-		type RuntimeEvent = <Polkadot as Chain>::RuntimeEvent;
-		type Referenda = <Polkadot as PolkadotPallet>::Referenda;
+	let ref_origins = vec![
+		Origin::StakingAdmin,
+		Origin::Treasurer,
+		Origin::FellowshipAdmin,
+		Origin::GeneralAdmin,
+		Origin::AuctionAdmin,
+		Origin::LeaseAdmin,
+		Origin::ReferendumCanceller,
+		Origin::ReferendumKiller,
+		Origin::SmallTipper,
+		Origin::BigTipper,
+		Origin::SmallSpender,
+		Origin::MediumSpender,
+		Origin::BigSpender,
+		Origin::WhitelistedCaller,
+		Origin::WishForChange,
+	];
+	let origins: Vec<(OriginKind, RuntimeOrigin)> = vec![
+		//(OriginKind::Native, Origin::ReferendumCanceller.into()),
+		(OriginKind::Superuser, <Polkadot as Chain>::RuntimeOrigin::root()),
+	];
 
-		let asset_kind: VersionedLocatableAsset = VersionedLocatableAsset::V4 {
-			location: Location::new(0, Here),
-			asset_id: Location::here().into(),
-		};
+	let mut ref_index: u32 = 0;
 
-		// Create proposal on which referendum will be based
-		let treasury_call = RuntimeCall::Treasury(pallet_treasury::Call::<Runtime>::spend {
-			asset_kind: Box::new(asset_kind),
-			amount: 100 * DOT,
-			beneficiary: Box::new(Location::new(0, Into::<[u8; 32]>::into(polkadot_bob.clone())).into()),
-			valid_from: None,
-		});
+	for (origin_kind, origin) in origins {
+		for ref_origin in &ref_origins {
+			Polkadot::execute_with(|| {				
+				type Runtime = <Polkadot as Chain>::Runtime;
+				type RuntimeCall = <Polkadot as Chain>::RuntimeCall;
+				type RuntimeEvent = <Polkadot as Chain>::RuntimeEvent;
+				type Referenda = <Polkadot as PolkadotPallet>::Referenda;
 
-		let proposal = <Preimage as StorePreimage>::bound(treasury_call).unwrap();
+				let alice_origin = <Polkadot as Chain>::RuntimeOrigin::signed(polkadot_alice.clone());
 
-		let alice_origin = <Polkadot as Chain>::RuntimeOrigin::signed(polkadot_alice.clone());
-		let bob_origin = <Polkadot as Chain>::RuntimeOrigin::signed(polkadot_alice.clone());
+				// Create proposal on which the referendum will be based
+				let call = RuntimeCall::System(frame_system::Call::<Runtime>::remark {
+					remark: vec![42, 42, 42]
+				});
 
-		// Submit referendum
-		assert_ok!(
-			Referenda::submit(
-				alice_origin.clone(),
-				Box::new(Origin::SmallTipper.into()),
-				proposal,
-				DispatchTime::After(0u32.into())
-			)
-		);
+				let proposal = <Preimage as StorePreimage>::bound(call).unwrap();
 
-		let ref_index: u32 = 0;
-		assert_expected_events!(
-			Polkadot,
-			vec![
-				RuntimeEvent::Referenda(pallet_referenda::Event::Submitted { index: 0, .. }) => {},
-			]
-		);
+				// Submit referendum
+				assert_ok!(
+					Referenda::submit(
+						alice_origin.clone(),
+						Box::new(Origin::SmallTipper.into()),
+						proposal,
+						DispatchTime::At(0u32.into())
+					)
+				);
 
-		// Place decision deposit
-		assert_ok!(
-			Referenda::place_decision_deposit(
-				bob_origin.clone(),
-				ref_index
-			)
-		);
+				assert_expected_events!(
+					Polkadot,
+					vec![
+						RuntimeEvent::Referenda(pallet_referenda::Event::Submitted { index: ref_index, .. }) => {},
+					]
+				);
 
-		assert_expected_events!(
-			Polkadot,
-			vec![
-				RuntimeEvent::Referenda(pallet_referenda::Event::DecisionDepositPlaced { index: 0, .. }) => {},
-			]
-		);
+/* 				// Cancel referendum
+				assert_ok!(
+					Referenda::cancel(
+						origin.clone(),
+						ref_index
+					)
+				);
 
-		let k = <Polkadot as Chain>::System::block_number();
-		log::warn!(target: "integration_tests", "Current block number: {:?}", k);
+				assert_expected_events!(
+					Polkadot,
+					vec![
+						RuntimeEvent::Referenda(pallet_referenda::Event::Cancelled {
+							index: ref_index,
+							tally:
+							Tally {
+								ayes: 0,
+								nays: 0,
+								support: 0,
+								..
+							} 
+						}) => {},
+					]
+				);
+*/
 
-		// Vote on the referendum - use all three different voting kinds.
+				let cancel_ref_call =
+				RuntimeCall::Referenda(pallet_referenda::Call::<Runtime>::cancel { index: ref_index });
 
-		let charlie_origin = <Polkadot as Chain>::RuntimeOrigin::signed(polkadot_charlie.clone());
-		let aye_vote = 5 * DOT;
+				let xcm_message = RuntimeCall::XcmPallet(pallet_xcm::Call::<Runtime>::send {
+					dest: bx!(VersionedLocation::from(Location::new(0, Here))),
+					message: bx!(VersionedXcm::from(Xcm(vec![
+						UnpaidExecution { weight_limit: Unlimited, check_origin: None },
+						Transact {
+							origin_kind,
+							require_weight_at_most: Weight::from_parts(100_000_000_000, 1_000_000),
+							call: cancel_ref_call.encode().into(),
+						}
+					]))),
+				});
+		
+				assert_ok!(xcm_message.dispatch(origin.clone()));
+		
+				assert_expected_events!(
+					Polkadot,
+					vec![
+						RuntimeEvent::XcmPallet(pallet_xcm::Event::Sent { .. }) => {},
+					]
+				);
 
-		assert_ok!(
-			ConvictionVoting::vote(
-				charlie_origin.clone(),
-				ref_index,
-				AccountVote::Standard {
-					vote: Vote {
-						aye: true, conviction: Conviction::Locked1x
-					},
-					balance: aye_vote
-				}
-			)
-		);
+			});
 
-		assert_expected_events!(
-			Polkadot,
-			vec![
-				RuntimeEvent::ConvictionVoting(pallet_conviction_voting::Event::Voted { vote: AccountVote::Standard { .. }, .. }) => {},
-			]
-		);
-
-		// Split voting
-
-		let dave_origin = <Polkadot as Chain>::RuntimeOrigin::signed(polkadot_dave.clone());
-		let nay_vote = 1 * DOT;
-
-		assert_ok!(
-			ConvictionVoting::vote(
-				dave_origin.clone(),
-				ref_index,
-				AccountVote::Split {
-					aye: aye_vote,
-					nay: nay_vote
-				}
-			)
-		);
-
-		assert_expected_events!(
-			Polkadot,
-			vec![
-				RuntimeEvent::ConvictionVoting(pallet_conviction_voting::Event::Voted { vote: AccountVote::Split { .. }, .. }) => {},
-			]
-		);
-
-		// Split abstain voting
-
-		let eve_origin = <Polkadot as Chain>::RuntimeOrigin::signed(polkadot_eve.clone());
-		let abstain_vote = 2 * DOT;
-
-		assert_ok!(
-			ConvictionVoting::vote(
-				eve_origin.clone(),
-				ref_index,
-				AccountVote::SplitAbstain {
-					aye: aye_vote,
-					nay: nay_vote,
-					abstain: abstain_vote
-				}
-			)
-		);
-
-		assert_expected_events!(
-			Polkadot,
-			vec![
-				RuntimeEvent::ConvictionVoting(pallet_conviction_voting::Event::Voted { vote: AccountVote::SplitAbstain { .. }, .. }) => {},
-			]
-		);
-
-		let k = <Polkadot as Chain>::System::block_number();
-		log::warn!(target: "integration_tests", "Current block number: {:?}", k);
-
-		let info = ReferendumInfoFor::<Runtime, ()>::get(ref_index).unwrap();
-		log::warn!(target: "integration_tests", "Referendum info: {:?}", info);
-
-		let root = <Polkadot as Chain>::RuntimeOrigin::root();
-		//  Nudge the referendum to its next state
-		assert_ok!(
-			Referenda::nudge_referendum(root, ref_index)
-		);
-
-		// Advance to the end of the voting period
-		<Polkadot as Chain>::System::set_block_number(k + 11);
-
-		let k = <Polkadot as Chain>::System::block_number();
-		log::warn!(target: "integration_tests", "Current block number: {:?}", k);
-
-		System::set_block_number(current_block + 1);
-		System::on_initialize(current_block + 1);
-
-		let info = ReferendumInfoFor::<Runtime, ()>::get(ref_index).unwrap();
-		log::warn!(target: "integration_tests", "Referendum info: {:?}", info);
-	});
-
+			ref_index += 1;
+		}
+	}
 }
